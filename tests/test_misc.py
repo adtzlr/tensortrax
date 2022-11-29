d80dcb6ae0d8b6ec0a1d739cfb0bf54a99d6f078
Andreas Dutzler <a.dutzler@gmail.com>
2022-11-29 16:33:49 +0100

    Update test_misc.py

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -14,22 +14,26 @@ def ogden(F, mu=1, alpha=2):
     C = F.T() @ F
     J = tm.det(F)
     λ = tm.sqrt(tm.eigvalsh(J ** (-2 / 3) * C))
-    return sum(1 / alpha * (λ**alpha - 1))
+    return tm.sum(1 / alpha * (λ**alpha - 1))
 
 
-def test_tensor():
+def test_gradient_hessian():
 
     np.random.seed(125161)
     dudX = np.random.rand(3, 3, 8, 50) / 10
     F = tm._eye(dudX) + dudX
 
     for fun in [neo_hooke, ogden]:
-        d2WdF2, dWdF, W = tr.hessian(neo_hooke)(F)
+        dwdf, w = tr.gradient(fun)(F)
+        d2WdF2, dWdF, W = tr.hessian(fun)(F)
 
         assert W.shape == (8, 50)
         assert dWdF.shape == (3, 3, 8, 50)
         assert d2WdF2.shape == (3, 3, 3, 3, 8, 50)
 
+        assert np.allclose(w, W)
+        assert np.allclose(dwdf, dWdF)
+
 
 if __name__ == "__main__":
-    test_tensor()
+    test_gradient_hessian()
