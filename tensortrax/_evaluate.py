5b01e1daa4fadc0e048e1e864a1998dd3f7a3c1e
Andreas Dutzler <a.dutzler@gmail.com>
2022-12-06 12:53:01 +0100

    remove joblib

--- tensortrax/_evaluate.py
+++ tensortrax/_evaluate.py
@@ -8,13 +8,13 @@ r"""
                                 ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝  
 """
 
-from joblib import delayed, Parallel, cpu_count
+from threading import Thread
 import numpy as np
 
 from ._tensor import Tensor, f, δ, Δδ
 
 
-def function(fun, ntrax=0, njobs=cpu_count()):
+def function(fun, ntrax=0, parallel=False):
     "Evaluate a scalar-valued function."
 
     def evaluate_function(x, *args, **kwargs):
@@ -23,7 +23,7 @@ def function(fun, ntrax=0, njobs=cpu_count()):
     return evaluate_function
 
 
-def gradient(fun, ntrax=0, njobs=cpu_count()):
+def gradient(fun, ntrax=0, parallel=False):
     "Evaluate the gradient of a scalar-valued function."
 
     def evaluate_gradient(x, *args, **kwargs):
@@ -33,24 +33,36 @@ def gradient(fun, ntrax=0, njobs=cpu_count()):
 
         fx = np.zeros((1, *t.trax))
         dfdx = np.zeros((t.size, *t.trax))
-        δx = np.eye(t.size)
+        δx = Δx = np.eye(t.size)
 
-        def kernel(a, x, δx, *args, **kwargs):
-            t = Tensor(x, δx=δx[a], Δx=δx[a], ntrax=ntrax)
+        def kernel(a, x, δx, Δx, args, kwargs):
+            t = Tensor(x, δx=δx[a], Δx=Δx[a], ntrax=ntrax)
             func = fun(t, *args, **kwargs)
             fx[:] = f(func)
             dfdx[a] = δ(func)
-
-        Parallel(n_jobs=njobs, backend="threading")(
-            delayed(kernel)(a, x, δx, *args, **kwargs) for a in indices
-        )
+        
+        if not parallel:
+            for a in indices:
+                kernel(a, x, δx, Δx, args, kwargs)
+                
+        else:
+            threads = [
+                Thread(target=kernel, args=(a, x, δx, Δx, args, kwargs))
+                for a in indices
+            ]
+            
+            for th in threads:
+                th.start()
+    
+            for th in threads:
+                th.join()
 
         return np.array(dfdx).reshape(*t.shape, *t.trax), fx[0]
 
     return evaluate_gradient
 
 
-def hessian(fun, ntrax=0, njobs=cpu_count()):
+def hessian(fun, ntrax=0, parallel=False):
     "Evaluate the hessian of a scalar-valued function."
 
     def evaluate_hessian(x, *args, **kwargs):
@@ -63,16 +75,28 @@ def hessian(fun, ntrax=0, njobs=cpu_count()):
         d2fdx2 = np.zeros((t.size, t.size, *t.trax))
         δx = Δx = np.eye(t.size)
 
-        def kernel(a, b, x, δx, *args, **kwargs):
+        def kernel(a, b, x, δx, Δx, args, kwargs):
             t = Tensor(x, δx=δx[a], Δx=Δx[b], ntrax=ntrax)
             func = fun(t, *args, **kwargs)
             fx[:] = f(func)
             dfdx[a] = δ(func)
             d2fdx2[a, b] = d2fdx2[b, a] = Δδ(func)
-
-        Parallel(n_jobs=njobs, backend="threading")(
-            delayed(kernel)(a, b, x, δx, *args, **kwargs) for a, b in indices
-        )
+        
+        if not parallel:
+            for a, b in indices:
+                kernel(a, b, x, δx, Δx, args, kwargs)
+                
+        else:
+            threads = [
+                Thread(target=kernel, args=(a, b, x, δx, Δx, args, kwargs))
+                for a, b in indices
+            ]
+            
+            for th in threads:
+                th.start()
+    
+            for th in threads:
+                th.join()
 
         return (
             np.array(d2fdx2).reshape(*t.shape, *t.shape, *t.trax),
@@ -83,7 +107,7 @@ def hessian(fun, ntrax=0, njobs=cpu_count()):
     return evaluate_hessian
 
 
-def gradient_vector_product(fun, ntrax=0, njobs=cpu_count()):
+def gradient_vector_product(fun, ntrax=0, parallel=False):
     "Evaluate the gradient-vector-product of a function."
 
     def evaluate_gradient_vector_product(x, δx, *args, **kwargs):
@@ -92,7 +116,7 @@ def gradient_vector_product(fun, ntrax=0, njobs=cpu_count()):
     return evaluate_gradient_vector_product
 
 
-def hessian_vector_product(fun, ntrax=0, njobs=cpu_count()):
+def hessian_vector_product(fun, ntrax=0, parallel=False):
     "Evaluate the gradient-vector-product of a function."
 
     def evaluate_hessian_vector_product(x, δx, Δx, *args, **kwargs):
