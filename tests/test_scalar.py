61170024af5cd02a7f55fbdcff16b43a80ee9cee
Andreas Dutzler <a.dutzler@gmail.com>
2022-12-09 14:58:57 +0100

    Create test_scalar.py

---
+++ tests/test_scalar.py
@@ -0,0 +1,30 @@
+import tensortrax as tr
+import tensortrax.math as tm
+import numpy as np
+
+
+def fun(x, y):
+    return x**2 / y + x * tm.log(y)
+
+
+def test_scalar():
+
+    np.random.seed(6574)
+    x = np.random.rand(100)
+
+    np.random.seed(54234)
+    y = np.random.rand(100)
+
+    h, g, f = tr.hessian(fun, wrt=0, ntrax=1)(x, y)
+
+    assert np.allclose(g, 2 * x / y + np.log(y))
+    assert np.allclose(h, 2 / y)
+
+    h, g, f = tr.hessian(fun, wrt=1, ntrax=1)(x, y)
+
+    assert np.allclose(g, -(x**2) / y**2 + x / y)
+    assert np.allclose(h, 2 * x**2 / y**3 - x / y**2)
+
+
+if __name__ == "__main__":
+    test_scalar()
