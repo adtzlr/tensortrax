f5f32757f6848ecd07a68895a5584b6655960ac1
Andreas Dutzler <a.dutzler@gmail.com>
2024-03-04 10:56:07 +0100

    Add a backward-compatible fix for modules `math._linalg` and `math._special`
    they are now located in `math.linalg` and `math.special`

---
+++ src/tensortrax/math/_special.py
@@ -0,0 +1,21 @@
+from .special import (
+    ddot,
+    dev,
+    from_triu_1d,
+    from_triu_2d,
+    sym,
+    tresca,
+    triu_1d,
+    von_mises,
+)
+
+__all__ = [
+    "ddot",
+    "dev",
+    "from_triu_1d",
+    "from_triu_2d",
+    "sym",
+    "tresca",
+    "triu_1d",
+    "von_mises",
+]
